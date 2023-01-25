789e0857bf666212236128960c1f00abbe45d3db
Lucian Amos <57368744+acunil@users.noreply.github.com>
2023-01-25 20:46:46 +0000

    Compare Hands (#7)
    adds Hand.compare with tests
    
    adds CompareHandsUtil.getWinningHands with tests
    
    adds overloaded getBestHand with string label input

--- src/test/java/org/example/base/domain/Components/HandTest.java
+++ src/test/java/org/example/base/domain/Components/HandTest.java
@@ -1,7 +1,6 @@
 package org.example.base.domain.Components;
 
 import lombok.val;
-import org.example.base.domain.Enums.HandType;
 import org.example.base.domain.Helpers.GeneralUtil;
 import org.example.base.domain.Helpers.IdentifyHandUtil;
 import org.junit.jupiter.api.Test;
@@ -14,8 +13,51 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 class HandTest {
 
+    public static final Hand FULL_HOUSE_HAND = IdentifyHandUtil.getBestHand("KS,KC,KH,4H,4D,9S,TH");
+    public static final Hand PAIR_HAND = IdentifyHandUtil.getBestHand("AS,AC,4D,5C,7H,8S,KC");
+
     @Test
-    void compare() {
+    void compare_givenHandIsHigher_returns1() {
+        Integer result = FULL_HOUSE_HAND.compare(PAIR_HAND);
+        assertThat(result).isEqualTo(1);
+    }
+
+    @Test
+    void compare_givenHandIsLower_returnsMinus1() {
+        Integer result = PAIR_HAND.compare(FULL_HOUSE_HAND);
+        assertThat(result).isEqualTo(-1);
+    }
+
+    @Test
+    void compare_givenHandHasIdenticalValues_returns0() {
+        Hand targetPairHand = IdentifyHandUtil.getBestHand("AH,AD,4D,5C,7H,8S,KC");
+        Integer result = PAIR_HAND.compare(targetPairHand);
+        assertThat(result).isEqualTo(0);
+    }
+
+    @Test
+    void compare_givenHandHasIdenticalCards_returns0() {
+        Integer result = PAIR_HAND.compare(PAIR_HAND);
+        assertThat(result).isEqualTo(0);
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {
+            "KH,KD,TH,TS,4H,7D,9C:KH,KD,JD,JC,4H,7D,9C:-1",
+            "KH,KD,KS,KC,JH,7D,9C:KH,KD,KS,KC,TH,7D,9C:1",
+            "6S,5S,4S,3S,2S,KS,QS:7S,6S,5S,4S,3S,KS,QS:-1",
+            "QD,QC,AS,9C,8H,5S,4S:QH,QD,AS,TC,8H,5S,4S:-1",
+            "4H,7H,8H,AH,JH,KD,2D:4H,7H,8H,KH,JH,KD,2D:1",
+    }, delimiter = ':')
+    void compare_givenHandTypeIsSame_returnsExpectedBasedOnProgressiveHighCard(
+        String handInput,
+        String targetHandInput,
+        Integer expected
+    ) {
+        Hand hand = IdentifyHandUtil.getBestHand(handInput);
+        Hand targetHand = IdentifyHandUtil.getBestHand(targetHandInput);
+        Integer result = hand.compare(targetHand);
+        assertThat(result).isEqualTo(expected);
     }
 
     @ParameterizedTest
